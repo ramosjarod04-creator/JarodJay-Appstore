ac13c730f1bc8780dd66947419eb196e5f24ac08
reinelgiro123 <rci.bsis.competentegiro@gmail.com>
2026-07-21 20:48:32 +0800

    Fix requirements.txt encoding without BOM

--- appstore_project/settings.py
+++ appstore_project/settings.py
@@ -134,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloud cloudinaryStorage'
